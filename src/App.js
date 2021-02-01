a9867b5554c7feaca64f585030aa2316c3a3bd98
unknown <nitinfab0@gmail.com>
2021-02-01 20:35:14 +0530

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import FeatureScreen from './screens/FeatureScreen';
+import ContactUs from './screens/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/" exact>
+         <FeatureScreen/>
+      </Route>
+      <Route path="/contacts" exact>
+        <ContactUs/>
+      </Route>
+      <Redirect to="/"/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/screens/FeatureScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FeatureScreen() {
+    return (
+    <div>
+      <h1> I am Feature Screen </h1>
+    </div> 
+    );
+}
+
+export default FeatureScreen;
